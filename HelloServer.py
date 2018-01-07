f6ff8cd286806600a45345320a936bf203c66bb1
Manish Kumar <ios.curator@gmail.com>
2018-01-07 15:28:21 +0530

    Initial commit with the working GET and POST request

---
+++ HelloServer.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+#
+# The *hello server* is an HTTP server that responds to a GET request by
+# sending back a friendly greeting.  Run this program in your terminal and
+# access the server at http://localhost:8000 in your browser.
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import parse_qs
+
+sampleHtmlForm = '''
+            <!DOCTYPE html>
+            <title>Message Board</title>
+            <form method="POST" action="http://localhost:8000/">
+                <textarea name="message"></textarea>
+                <br>
+                <button type="submit">Post it!</button>
+            </form>
+        '''
+
+class HelloHandler(BaseHTTPRequestHandler):
+
+    def processGetResponse(self):
+        # Now, write the response body.
+        self.wfile.write(self.path[1:].encode())
+    
+    def showForm(self):
+        # Now, write the response body.
+        self.wfile.write(sampleHtmlForm.encode())
+
+    def do_GET(self):
+        # First, send a 200 OK response.
+        self.send_response(200)
+
+        # Then send headers.
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+        print("Path is " +self.path[1:])
+        
+
+        if self.path != '/':
+            HelloHandler.processGetResponse(self)
+        else:
+            HelloHandler.showForm(self)
+
+    def do_POST(self):
+         #Read the value of content-length
+         length = int(self.headers.get('content-length', 0))
+         data = self.rfile.read(length).decode()
+
+         parsed_data = parse_qs(data)
+
+         magic = parsed_data['message'][0]
+         # First, send a 200 OK response.
+         self.send_response(200)
+
+         # Then send headers.
+         self.send_header('Content-type', 'text/plain; charset=utf-8')
+         self.end_headers()
+
+         # Now, write the response body.
+         self.wfile.write(magic.encode())
+
+if __name__ == '__main__':
+    server_address = ('', 8000)  # Serve on all addresses, port 8000.
+    httpd = HTTPServer(server_address, HelloHandler)
+    httpd.serve_forever()
